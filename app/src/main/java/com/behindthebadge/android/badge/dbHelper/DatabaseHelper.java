a7b0348276ba5732cd85a65f69120db98c6b6764
us50416 <peggy.wiltz@premera.com>
2017-12-11 10:12:18 -0800

    pw working on database

--- app/src/main/java/com/behindthebadge/android/badge/dbHelper/DatabaseHelper.java
+++ app/src/main/java/com/behindthebadge/android/badge/dbHelper/DatabaseHelper.java
@@ -24,6 +24,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Logcat tag
     private static final String LOG = "DatabaseHelper";
+    private static final int DATABASE_VERSION = 1;
 
     public static final String DATABASE_NAME = "BehindTheBadge.db";
     public static final String RESPONDERS_TABLE_NAME = "responders";
@@ -40,19 +41,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String EVENT_DATE = "DATE";
 
     public static final String RESPONDERS_EVENTS_TABLE_NAME = "responders_events";
-    public static final String RESPONDERS_EVENTS_RESPONDERS_ID = "RESPONDERS_ID";
-    public static final String RESPONDERS_EVENTS_EVENTS_ID = "EVENTS_ID";
+    public static final String RESPONDERS_EVENTS_RESPONDERS_ID = "RESPONDER_ID";
+    public static final String RESPONDERS_EVENTS_EVENTS_ID = "EVENT_ID";
 
     public static final String RESPONDERS_RESPONSES_TABLE_NAME = "responders_responses";
-    public static final String RESPONDERS_RESPONDES_RESPONDERS_ID = "RESPONDERS_ID";
+    public static final String RESPONDERS_RESPONDES_RESPONDERS_ID = "RESPONDER_ID";
     public static final String RESPONDERS_RESPONSES_RESPONSE_ID = "RESPONSE_ID";
 
     public static final String RESPONSES_EVENTS_TABLE_NAME = "responses_events";
-    public static final String RESPONSES_EVENTS_RESPONDERS_ID = "RESPONDERS_ID";
+    public static final String RESPONSES_EVENTS_RESPONDERS_ID = "RESPONDER_ID";
     public static final String RESPONSES_EVENTS_RESPONSE_ID = "RESPONSE_ID";
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
+    }
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
@@ -80,7 +85,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     /*
      * Creating a responder
      */
-    public long createResponder(Responder responder, long[] responder_ids) {
+    public long createResponder(Responder responder) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -94,8 +99,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     /*
- * get single todo
- */
+     * get single todo
+    */
     public Responder getResponder(long responder_id) {
         SQLiteDatabase db = this.getReadableDatabase();
 
@@ -134,7 +139,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 resp.setName((c.getString(c.getColumnIndex(RESPONDERS_NAME))));
                 resp.setPhone(c.getString(c.getColumnIndex(RESPONDERS_PHONE)));
 
-                // adding to todo list
+                // adding to responders list
                 responders.add(resp);
             } while (c.moveToNext());
         }
@@ -142,7 +147,356 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return responders;
     }
 
+    /*
+     * getting all responders under single event
+     * */
+    public List<Responder> getAllRespondersByEvent(String event_name) {
+        List<Responder> responders = new ArrayList<Responder>();
 
+        String selectQuery = "SELECT  * FROM " + RESPONDERS_TABLE_NAME + " resp, "
+                + EVENTS_TABLE_NAME + " evt, " + RESPONDERS_EVENTS_TABLE_NAME + " resp_evt WHERE evt."
+                + EVENT_NAME + " = '" + event_name + "'" + " AND evt." + KEY_ID
+                + " = " + "resp_evt." + RESPONDERS_EVENTS_EVENTS_ID + " AND resp." + KEY_ID + " = "
+                + "resp_evt." + RESPONDERS_EVENTS_RESPONDERS_ID;
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Responder td = new Responder();
+                td.setId(c.getInt((c.getColumnIndex(KEY_ID))));
+                td.setName((c.getString(c.getColumnIndex(RESPONDERS_NAME))));
+                td.setPhone(c.getString(c.getColumnIndex(RESPONDERS_PHONE)));
+
+                responders.add(td);
+            } while (c.moveToNext());
+        }
+
+        return responders;
+    }
+
+    /*
+    * Updating a responder
+    */
+    public int updateResponder(Responder responder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONDERS_NAME, responder.getName());
+        values.put(RESPONDERS_PHONE, responder.getPhone());
+
+        // updating row
+        return db.update(RESPONDERS_TABLE_NAME, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(responder.getId()) });
+    }
+
+    /*
+    * Deleting a responder
+    */
+    public void deleteResponder(long responder_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(RESPONDERS_TABLE_NAME, KEY_ID + " = ?",
+                new String[] { String.valueOf(responder_id) });
+    }
+
+    /*
+    * Creating an event
+    */
+    public long createResponse(Response response, long responder_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONSES_RESPONSE, response.getResponse());
+        values.put(RESPONSES_EVENT, response.getEvent());
+
+        // insert row
+        long _id = db.insert(RESPONSES_TABLE_NAME, null, values);
+
+        createResponderResponse(responder_id, _id);
+
+        return _id;
+    }
+
+    /*
+     * Creating ResponderResponse
+     */
+    public long createResponderResponse(long responder_id, long response_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONDERS_RESPONDES_RESPONDERS_ID, responder_id);
+        values.put(RESPONDERS_RESPONDES_RESPONDERS_ID, response_id);
+
+        long id = db.insert(RESPONDERS_RESPONSES_TABLE_NAME, null, values);
+
+        return id;
+    }
+
+    /*
+    * get single event
+    */
+    public Response getResponse(long response_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + RESPONSES_TABLE_NAME + " WHERE "
+                + KEY_ID + " = " + response_id;
+
+        Log.e(LOG, selectQuery);
+
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c != null)
+            c.moveToFirst();
+
+        Response response = new Response();
+        response.setId(c.getInt(c.getColumnIndex(KEY_ID)));
+        response.setEvent(c.getString(c.getColumnIndex(RESPONSES_EVENT)));
+        response.setResponse(c.getInt(c.getColumnIndex(RESPONSES_RESPONSE)));
+
+        return response;
+    }
+
+    public List<Response> getAllResponses() {
+        List<Response> responses = new ArrayList<Response>();
+        String selectQuery = "SELECT  * FROM " + RESPONSES_TABLE_NAME;
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Response response = new Response();
+                response.setId(c.getInt((c.getColumnIndex(KEY_ID))));
+                response.setResponse((c.getInt(c.getColumnIndex(RESPONSES_RESPONSE))));
+                response.setEvent(c.getString(c.getColumnIndex(RESPONSES_EVENT)));
+
+                // adding to responses list
+                responses.add(response);
+            } while (c.moveToNext());
+        }
+
+        return responses;
+    }
+
+    /*
+     * getting all responses under single responder
+     * */
+    public List<Event> getAllResponsesByResponder(String responder_name) {
+        List<Event> events = new ArrayList<Event>();
+
+        String selectQuery = "SELECT  * FROM " + EVENTS_TABLE_NAME + " evt, "
+                + EVENTS_TABLE_NAME + " resp, " + RESPONDERS_EVENTS_TABLE_NAME + " resp_evt WHERE resp."
+                + EVENT_NAME + " = '" + responder_name + "'" + " AND resp." + KEY_ID
+                + " = " + "resp_evt." + RESPONDERS_EVENTS_EVENTS_ID + " AND evt." + KEY_ID + " = "
+                + "resp_evt." + RESPONDERS_EVENTS_RESPONDERS_ID;
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Event evt = new Event();
+                evt.setId(c.getInt((c.getColumnIndex(KEY_ID))));
+                evt.setEventName((c.getString(c.getColumnIndex(EVENT_NAME))));
+                evt.setDate(c.getString(c.getColumnIndex(EVENT_DATE)));
+
+                events.add(evt);
+            } while (c.moveToNext());
+        }
+
+        return events;
+    }
+
+    /*
+     * Updating a response
+     */
+    public int updateResponses(Response response) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONSES_EVENT, response.getEvent());
+        values.put(RESPONSES_RESPONSE, response.getResponse());
+
+        // updating row
+        return db.update(EVENTS_TABLE_NAME, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(response.getId()) });
+    }
+
+    /*
+     * Deleting a response
+     */
+    public void deleteResponse(long response_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(RESPONSES_TABLE_NAME, KEY_ID + " = ?",
+                new String[] { String.valueOf(response_id) });
+    }
+
+     /*
+     * Creating an event
+     */
+    public long createEvent(Event event) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(EVENT_NAME, event.getName());
+        values.put(EVENT_DATE, event.getDate());
+
+        // insert row
+        long _id = db.insert(EVENTS_TABLE_NAME, null, values);
+
+        return _id;
+    }
+
+    /*
+    * get single event
+    */
+    public Event getEvent(long event_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + EVENTS_TABLE_NAME + " WHERE "
+                + KEY_ID + " = " + event_id;
+
+        Log.e(LOG, selectQuery);
+
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c != null)
+            c.moveToFirst();
+
+        Event event = new Event();
+        event.setId(c.getInt(c.getColumnIndex(KEY_ID)));
+        event.setEventName((c.getString(c.getColumnIndex(EVENT_NAME))));
+        event.setDate(c.getString(c.getColumnIndex(EVENT_DATE)));
+
+        return event;
+    }
+
+    public List<Event> getAllEvents() {
+        List<Event> events = new ArrayList<Event>();
+        String selectQuery = "SELECT  * FROM " + EVENTS_TABLE_NAME;
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Event event = new Event();
+                event.setId(c.getInt((c.getColumnIndex(KEY_ID))));
+                event.setEventName((c.getString(c.getColumnIndex(EVENT_NAME))));
+                event.setDate(c.getString(c.getColumnIndex(EVENT_DATE)));
+
+                // adding to events list
+                events.add(event);
+            } while (c.moveToNext());
+        }
+
+        return events;
+    }
+
+    /*
+     * getting all events under single responder
+     * */
+    public List<Event> getAllEventsByResponder(String responder_name) {
+        List<Event> events = new ArrayList<Event>();
+
+        String selectQuery = "SELECT  * FROM " + EVENTS_TABLE_NAME + " evt, "
+                + EVENTS_TABLE_NAME + " resp, " + RESPONDERS_EVENTS_TABLE_NAME + " resp_evt WHERE resp."
+                + EVENT_NAME + " = '" + responder_name + "'" + " AND resp." + KEY_ID
+                + " = " + "resp_evt." + RESPONDERS_EVENTS_EVENTS_ID + " AND evt." + KEY_ID + " = "
+                + "resp_evt." + RESPONDERS_EVENTS_RESPONDERS_ID;
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Event evt = new Event();
+                evt.setId(c.getInt((c.getColumnIndex(KEY_ID))));
+                evt.setEventName((c.getString(c.getColumnIndex(EVENT_NAME))));
+                evt.setDate(c.getString(c.getColumnIndex(EVENT_DATE)));
+
+                events.add(evt);
+            } while (c.moveToNext());
+        }
+
+        return events;
+    }
+
+    /*
+     * Updating an event
+     */
+    public int updateEvent(Event event) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(EVENT_NAME, event.getName());
+        values.put(EVENT_DATE, event.getDate());
+
+        // updating row
+        return db.update(EVENTS_TABLE_NAME, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(event.getId()) });
+    }
+
+    /*
+ * Deleting an event
+ */
+    public void deleteEvent(long event_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(EVENTS_TABLE_NAME, KEY_ID + " = ?",
+                new String[] { String.valueOf(event_id) });
+    }
+
+    /*
+     * Creating responder_event
+     */
+    public long createResponderEvent(long responder_id, long event_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONDERS_EVENTS_RESPONDERS_ID, responder_id);
+        values.put(RESPONDERS_EVENTS_EVENTS_ID, event_id);
+
+        long id = db.insert(RESPONDERS_EVENTS_TABLE_NAME, null, values);
+
+        return id;
+    }
+
+     /*
+     * Updating a responder event
+     */
+    public int updateResponderEvent(long id, long event_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(RESPONDERS_EVENTS_EVENTS_ID, event_id);
+
+        // updating row
+        return db.update(RESPONDERS_TABLE_NAME, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(id) });
+    }
+
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
+    }
 
     public static String getDateTime(){
         Date presentTime_Date = Calendar.getInstance().getTime();
--- app/src/main/java/com/behindthebadge/android/badge/screens/MainActivity.java
+++ app/src/main/java/com/behindthebadge/android/badge/screens/MainActivity.java
@@ -3,15 +3,23 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.behindthebadge.android.badge.R;
+import com.behindthebadge.android.badge.dbHelper.DatabaseHelper;
+import com.behindthebadge.android.badge.dbHelper.Event;
+import com.behindthebadge.android.badge.dbHelper.Responder;
+import com.behindthebadge.android.badge.dbHelper.Response;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     TabLayout tabLayout;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,64 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(1).setText(R.string.main_response_tab);
 
         viewPager.setAdapter(pageAdapter);
+
+        db = new DatabaseHelper(getApplicationContext());
+
+        // Creating events
+        Event event1 = new Event("Wyoming", "2/15/2018");
+        Event event2 = new Event("Chicago", "3/22/2018");
+        Event event3 = new Event("Houston", "5/25/2018");
+        Event event4 = new Event("Florida", "7/15/2018");
+
+        // Inserting tags in db
+        long event1_id = db.createEvent(event1);
+        long event2_id = db.createEvent(event2);
+        long event3_id = db.createEvent(event3);
+        long event4_id = db.createEvent(event4);
+
+        Log.d("Event Count", "Event Count: " + db.getAllEvents().size());
+
+        Log.d("Get Events", "Getting All Events");
+        List<Event> allEvents = db.getAllEvents();
+        for(Event event : allEvents) {
+            Log.d("Events", event.getName() + " " + event.getDate());
+        }
+
+        // Creating responders
+        Responder resp1 = new Responder("Steve Dallas", "425-555-1212");
+        Responder resp2 = new Responder("Milo Blom", "425-555-2323");
+        Responder resp3 = new Responder("Opus", "425-555-3434");
+        Responder resp4 = new Responder("Roscoe P. Coltrane", "425-555-5656");
+
+        // Inserting responders in db
+        long resp1_id = db.createResponder(resp1);
+        long resp2_id = db.createResponder(resp2);
+        long resp3_id = db.createResponder(resp3);
+        long resp4_id = db.createResponder(resp4);
+
+        Log.e("Responder Count", "Responder count: " + db.getAllResponders().size());
+
+        // Getting all responder names
+        Log.d("Get Responders", "Getting All Responders");
+
+        List<Responder> allResponders = db.getAllResponders();
+        for (Responder responder : allResponders) {
+            Log.d("Responder: ", responder.getName() + " " + responder.getPhone());
+        }
+
+        //Creating responses
+        Response r1 = new Response();
+        Response r2 = new Response();
+        Response r3 = new Response();
+        Response r4 = new Response();
+
+        //Inserting responses into db
+        long r1_id = db.createResponse(r1, resp1_id);
+        long r2_id = db.createResponse(r1, resp2_id);
+        long r3_id = db.createRes
+
+        // Don't forget to close database connection
+        db.closeDB();
     }
 
 
